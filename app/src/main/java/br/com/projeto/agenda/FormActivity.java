4f8b29fcf9a00e6b950b935b07d1a48c2437f842
lucashegomes <lucashegomes@gmail.com>
2019-11-27 23:31:13 -0200

    Students list and form to create their

---
+++ app/src/main/java/br/com/projeto/agenda/FormActivity.java
@@ -0,0 +1,47 @@
+package br.com.projeto.agenda;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_form, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_form_ok:
+                //Referencia a classe mãe deste comportamento (externa a ele) e exibe um rápido toast
+                Toast.makeText(FormActivity.this, "Aluno adicionado", Toast.LENGTH_SHORT).show();
+                finish(); // finaliza a activity sem correr o risco de o botão voltar trazer para este form
+
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
