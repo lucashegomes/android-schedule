cd265930f3b8c94e3134d2233b2696ad32b24d2b
alissonfelipe1234 <alissonfelipe1234@outlook.com>
2019-12-07 15:39:22 -0200

    Subindo CEP

--- app/src/main/java/br/com/projeto/agenda/FormActivity.java
+++ app/src/main/java/br/com/projeto/agenda/FormActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import br.com.projeto.agenda.models.Contact;
@@ -24,6 +25,24 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        final EditText cep = findViewById(R.id.etMain_cep);
+        final TextView resposta = findViewById(R.id.etMain_resposta);
+
+        Button btnBuscarCep = findViewById(R.id.btnMain_buscarCep);
+        btnBuscarCep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    CEP retorno = new HttpService(cep.getText().toString()).execute().get();
+                    retorno.setText(retorno.toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         helper = new FormHelper(this);
     }
